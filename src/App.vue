9c8f9d5314c5093f8f787017f14e76c54bb1be19
jenkey2011 <jenkey2011@163.com>
2018-01-01 23:00:26 +0800

    add tab

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <m-header></m-header>
+    <tab></tab>
     <keep-alive>
       <router-view></router-view>
     </keep-alive>
@@ -9,10 +10,12 @@
 
 <script type="text/ecmascript-6">
   import MHeader from 'components/m-header/m-header'
+  import Tab from 'components/tab/tab'
 
   export default {
     components: {
-      MHeader
+      MHeader,
+      Tab
     }
   }
 </script>
