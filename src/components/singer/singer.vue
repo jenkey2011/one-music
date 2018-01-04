e1ff524f2b85de08c9a2667987b7fac593a2595a
jenkey2011 <jenkey2011@163.com>
2018-01-04 17:17:31 +0800

    add singer

--- src/components/singer/singer.vue
+++ src/components/singer/singer.vue
@@ -1,15 +1,20 @@
 <template>
   <div class="singer">
-
+    <list-view :data="singers"></list-view>
   </div>
 </template>
 
 <script>
 import { getSingerList } from 'api/singer'
 import { ERR_OK } from 'api/config'
+import Singer from 'common/js/singer'
+import ListView from 'base/listview/listview'
+
+const HOT_NAME = '热门'
+const HOT_SINGER_LEN = 10
 
 export default {
-  data(){
+  data() {
     return {
       singers: []
     }
@@ -17,14 +22,69 @@ export default {
   created() {
     this._getSingerList()
   },
+  computed: {
+
+  },
   methods: {
     _getSingerList() {
+      const vm = this
       getSingerList().then((res) => {
         if (res.code === ERR_OK){
-          this.singers = res.data.list
+          // this.singers = this._normalizeSinger(res.data.list)
+          vm.singers = vm._normalizeSinger(res.data.list)
+          // this.singers.$set(i,{text:i+1});
+          console.log(vm.singers)
+          console.log(typeof(vm.singers))
         }
       })
+    },
+    _normalizeSinger(list) {
+      let map = {
+        hot: {
+          title: HOT_NAME,
+          items: []
+        }
+      }
+
+      list.forEach((item, index) => {
+        if (index < HOT_SINGER_LEN){
+          map.hot.items.push(new Singer({
+            id: item.Fsinger_mid,
+            name: item.Fsinger_name
+          }))
+        }
+        const key = item.Findex
+        if (!map[key]){
+          map[key] = {
+            title: key,
+            items: []
+          }
+        }
+        map[key].items.push(new Singer({
+          id: item.Fsinger_mid,
+          name: item.Fsinger_name
+        }))
+      })
+
+      var hot = []
+      var ret = []
+      for (let key in map) {
+        let val = map[key]
+        if (val.title.match(/[a-zA-Z]/)){
+          ret.push(val)
+        } else if (val.title === HOT_NAME){
+          hot.push(val)
+        }
+      }
+      ret.sort((a, b) => {
+        return a.title.charCodeAt(0) - b.title.charCodeAt(0)
+      })
+      // this.a = hot.concat(ret);
+      return hot.concat(ret)
     }
+  },
+  components: {
+    ListView
   }
 }
 </script>
