2657da49590c15712a93baeb21c15a0ee4396e23
jenkey2011 <jenkey2011@163.com>
2018-01-11 17:23:21 +0800

    singer detail

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,19 +1,22 @@
 <template>
   <transition name="slide">
     <!-- <div class="singer-detail"></div> -->
-    <music-list :songs="songs" :title="title" :bg-image="bgImage"></music-list>
+    <music-list :songs="songs"
+                :title="title"
+                :bg-image="bgImage">
+    </music-list>
   </transition>
 </template>
 
 <script>
+import MusicList from 'components/music-list/music-list'
 import { mapGetters } from 'vuex'
 import { getSingerDetail } from 'api/singer'
 import { ERR_OK } from 'api/config'
 import { createSong } from 'common/js/song'
-import MusicList from 'components/music-list/music-list'
 
 export default {
-  data() {
+  data () {
     return {
       songs: []
     }
@@ -22,19 +25,19 @@ export default {
     ...mapGetters([
       'singer'
     ]),
-    title() {
+    title () {
       return this.singer.name
     },
-    bgImage() {
+    bgImage () {
       return this.singer.pic
     }
   },
-  created() {
+  created () {
     this._getDetail()
   },
   methods: {
-    _getDetail() {
-      if (!this.singer.id){
+    _getDetail () {
+      if (!this.singer.id) {
         this.$router.push('/singer')
         return
       }
@@ -44,10 +47,10 @@ export default {
         }
       })
     },
-    _normalizeSongs(list) {
+    _normalizeSongs (list) {
       let ret = []
       list.forEach((item) => {
-        let {musicData} = item
+        let { musicData } = item
         if (musicData.songid && musicData.albummid) {
           ret.push(createSong(musicData))
         }
@@ -62,20 +65,17 @@ export default {
 </script>
 
 <style lang="stylus">
-  @import "~common/stylus/variable"
-
-  .singer-detail
-    position: fixed
-    left: 0
-    top: 0
-    right: 0
-    bottom: 0
-    z-index: 100
-    background: $color-background
-
-  .slide-enter-active, .slide-leave-active
-    transition: all 0.3s
-
-  .slide-enter, .slide-leave-to
-    transform: translate3d(100%, 0, 0)
+@import '~common/stylus/variable'
+.singer-detail
+  position fixed
+  left 0
+  top 0
+  right 0
+  bottom 0
+  z-index 100
+  background $color-background
+.slide-enter-active, .slide-leave-active
+  transition all 0.3s
+.slide-enter, .slide-leave-to
+  transform translate3d(100%, 0, 0)
 </style>
