fe64ce893ea5b6783d874ee4ab29af46b3fbf1bf
jenkey2011 <jenkey2011@163.com>
2018-01-12 17:17:50 +0800

    formmat

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -9,73 +9,73 @@
 </template>
 
 <script>
-import MusicList from 'components/music-list/music-list'
-import { mapGetters } from 'vuex'
-import { getSingerDetail } from 'api/singer'
-import { ERR_OK } from 'api/config'
-import { createSong } from 'common/js/song'
+  import MusicList from 'components/music-list/music-list'
+  import { mapGetters } from 'vuex'
+  import { getSingerDetail } from 'api/singer'
+  import { ERR_OK } from 'api/config'
+  import { createSong } from 'common/js/song'
 
-export default {
-  data () {
-    return {
-      songs: []
-    }
-  },
-  computed: {
-    ...mapGetters([
-      'singer'
-    ]),
-    title () {
-      return this.singer.name
-    },
-    bgImage () {
-      return this.singer.pic
-    }
-  },
-  created () {
-    this._getDetail()
-  },
-  methods: {
-    _getDetail () {
-      if (!this.singer.id) {
-        this.$router.push('/singer')
-        return
+  export default {
+    data () {
+      return {
+        songs: []
       }
-      getSingerDetail(this.singer.id).then((res) => {
-        if (res.code === ERR_OK) {
-          this.songs = this._normalizeSongs(res.data.list)
-        }
-      })
     },
-    _normalizeSongs (list) {
-      let ret = []
-      list.forEach((item) => {
-        let { musicData } = item
-        if (musicData.songid && musicData.albummid) {
-          ret.push(createSong(musicData))
+    computed: {
+      ...mapGetters([
+        'singer'
+      ]),
+      title () {
+        return this.singer.name
+      },
+      bgImage () {
+        return this.singer.pic
+      }
+    },
+    created () {
+      this._getDetail()
+    },
+    methods: {
+      _getDetail () {
+        if (!this.singer.id) {
+          this.$router.push('/singer')
+          return
         }
-      })
-      return ret
+        getSingerDetail(this.singer.id).then((res) => {
+          if (res.code === ERR_OK) {
+            this.songs = this._normalizeSongs(res.data.list)
+          }
+        })
+      },
+      _normalizeSongs (list) {
+        let ret = []
+        list.forEach((item) => {
+          let { musicData } = item
+          if (musicData.songid && musicData.albummid) {
+            ret.push(createSong(musicData))
+          }
+        })
+        return ret
+      }
+    },
+    components: {
+      MusicList
     }
-  },
-  components: {
-    MusicList
   }
-}
 </script>
 
 <style lang="stylus">
-@import '~common/stylus/variable'
-.singer-detail
-  position fixed
-  left 0
-  top 0
-  right 0
-  bottom 0
-  z-index 100
-  background $color-background
-.slide-enter-active, .slide-leave-active
-  transition all 0.3s
-.slide-enter, .slide-leave-to
-  transform translate3d(100%, 0, 0)
+  @import '~common/stylus/variable'
+  .singer-detail
+    position fixed
+    left 0
+    top 0
+    right 0
+    bottom 0
+    z-index 100
+    background $color-background
+  .slide-enter-active, .slide-leave-active
+    transition all 0.3s
+  .slide-enter, .slide-leave-to
+    transform translate3d(100%, 0, 0)
 </style>
