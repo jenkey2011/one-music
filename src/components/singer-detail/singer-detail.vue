f2703e9ac4ea417ac03d59641707caaa9ae53989
jenkey2011 <jenkey2011@163.com>
2018-01-17 21:49:01 +0800

    fix the singer-detail

--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -67,15 +67,15 @@
 <style lang="stylus">
   @import '~common/stylus/variable'
   .singer-detail
-    position fixed
-    left 0
-    top 0
-    right 0
-    bottom 0
-    z-index 100
-    background $color-background
+    position: fixed
+    left: 0
+    top: 0
+    right: 0
+    bottom: 0
+    z-index: 100
+    background: $color-background
   .slide-enter-active, .slide-leave-active
-    transition all 0.3s
+    transition: all 0.3s
   .slide-enter, .slide-leave-to
-    transform translate3d(100%, 0, 0)
+    transform: translate3d(100%, 0, 0)
 </style>
