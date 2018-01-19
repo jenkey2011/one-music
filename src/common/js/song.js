65a636c5210c156a44969da61c594166c384e7ae
jenkey2011 <jenkey2011@163.com>
2018-01-19 17:53:08 +0800

    parser the lyric

--- src/common/js/song.js
+++ src/common/js/song.js
@@ -1,5 +1,6 @@
 import { getLyric } from 'api/song'
 import { ERR_OK } from 'api/config'
+import { Base64 } from 'js-base64'
 
 export default class Song {
   constructor({ id, mid, singer, name, album, duration, image, url }) {
@@ -23,7 +24,8 @@ export default class Song {
         }
       }
       if (res.retcode === ERR_OK) {
-        this.lyric = res.lyric
+        this.lyric = Base64.decode(res.lyric)
+        console.log(this.lyric)
       }
     })
   }
